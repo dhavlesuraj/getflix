8a7d527a8cdd2be5f778df5abe8a25a0fe8051d0
surajdhavle1106 <suraj.dhawle@bytesigma.com>
2024-08-03 18:58:46 +0530

    initial commit

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SafeAreaView,Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <>
+      <SafeAreaView >
+        <Text>Profile</Text>
+      </SafeAreaView>
+    </>
+  );
+}
